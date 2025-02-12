7fe0d2bec04a204343176181bd417a39b9400d51
CodyC1998 <cody.collins@keyin.com>
2025-02-12 10:57:16 -0330

    MyPoint java code

--- MyPoint.java
+++ MyPoint.java
@@ -1,4 +1,39 @@
 public class MyPoint {
+    private int x;
+    private int y;
+
+    // Constructors
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters and Setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // to String method
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
 
     
 }
